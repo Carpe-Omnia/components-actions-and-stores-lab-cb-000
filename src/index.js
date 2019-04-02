2906855d1db9e3fb08e91ccd10f80179eb52b083
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 03:27:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
+
 import createStore  from './store.js';
-=======
+
 import { createStore } from './store.js';
->>>>>>> ef6efd94b7e7ec4d2619440398b2e93ba8d85fc7
+
 import bandReducer from './reducers/band_reducer.js'
 import BandInput from './components/band_input_component.js'
 import BandIndex  from './components/band_index_component.js';
